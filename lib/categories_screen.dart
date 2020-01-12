daa3ecd15f7bc37095a9b9d623f577bc50959314
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2020-01-13 00:56:32 +0530

    3rd commit

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        // THIS IS THE WIDTH OF THE EACH ITEM IN THE GRID VIEW
-        childAspectRatio: 3 / 2,
-        // THIS IS THE HEIGHT / WIDTH RATIO LIKE 300 HEIGHT AND 200 WIDTH IN THIS CASE
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Daily Meals",
+        ),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map(
+          (catData) {
+            CategoryItem(
+              catData.title,
+              catData.color,
+            );
+          },
+        ).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          // THIS IS THE WIDTH OF THE EACH ITEM IN THE GRID VIEW
+          childAspectRatio: 3 / 2,
+          // THIS IS THE HEIGHT / WIDTH RATIO LIKE 300 HEIGHT AND 200 WIDTH IN THIS CASE
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/dummy_data.dart
+++ lib/dummy_data.dart
@@ -1,44 +1,56 @@
+import 'package:flutter/material.dart';
+
 import './models/category.dart';
 
 const DUMMY_CATEGORIES = const [
   Category(
     id: 'c1',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c2',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c3',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c4',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c5',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c6',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c7',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c8',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c9',
     title: 'Italian',
+    color: Colors.blue,
   ),
   Category(
     id: 'c10',
     title: 'Italian',
+    color: Colors.blue,
   ),
 ];
